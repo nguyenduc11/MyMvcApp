b7caae11070368328508823a6ea1be66670678cf
nguyenduc <nguyenduc11@gmail.com>
2024-11-25 14:09:12 +0700

    organize code with areas

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMvcApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
